2ceb2de4e2707929f064b157be1d094093d0496c
ViktoriiaMassamba <Dyachenkovg84@gmail.com>
2023-08-02 02:48:07 +0300

    hw 7

--- HW_7/script-adv7.js
+++ HW_7/script-adv7.js
@@ -2,44 +2,47 @@
 скільки податків ви заплатите як IT-спеціаліст в якійсь з країн. Функція повинна
 викликатись через call та працювати з даними через this*/
 
-let myTex = {
-    getMyTaxes: function (salary){
+
+    function getMyTaxes (salary){
         return salary * this.tax;
     }
-}
 
 let salary = 5000;
 const ukraine = { tax: 0.195, middleSalary: 1789, vacancies: 11476 };
 const latvia = { tax: 0.25, middleSalary: 1586, vacancies: 3921 };
 const litva = { tax: 0.15, middleSalary: 1509, vacancies: 1114 };  
 
-console.log("1. Cкільки податків ви заплатите як IT-спеціаліст в якійсь з країн",  ((myTex.getMyTaxes.call(ukraine, salary)* 100) /100).toFixed(2) + " " + "грн." + "," + " в Латвіі - ", ((myTex.getMyTaxes.call(latvia, salary)* 100) /100).toFixed(2) + " " + "грн." + "," + " в Літві - ", ((myTex.getMyTaxes.call(litva, salary)* 100) /100).toFixed(2) + " " + "грн.") 
+console.log("1. Cкільки податків ви заплатите як IT-спеціаліст в якійсь з країн",  ((getMyTaxes.call(ukraine, salary)* 100) /100).toFixed(2) + " " + "грн." + "," + " в Латвіі - ", ((getMyTaxes.call(latvia, salary)* 100) /100).toFixed(2) + " " + "грн." + "," + " в Літві - ", ((getMyTaxes.call(litva, salary)* 100) /100).toFixed(2) + " " + "грн.") 
 
 /* 2. Створіть функцію getMiddleTaxes.call(country) -> number; – яка рахує
 скільки у середньому податків платять IT-спеціалісти у кожній країні. (tax *
 middleSalary). Функція повинна викликатись через call та працювати з даними
 через this*/
 
-let middleTex = {
-    getMiddleTex: function (country, salary){
+    function getMiddleTex (country, salary){
         return this.tax * this.middleSalary;
     }
-}
 
-console.log("2. Cкільки у середньому податків платять IT-спеціалісти в Україні - ",  ((middleTex.getMiddleTex.call(ukraine)* 100) /100).toFixed(2) + " " + "грн." + "," + " в Латвіі - ", ((middleTex.getMiddleTex.call(latvia)* 100) /100).toFixed(2) + " " + "грн." + "," + " в Літві - ", ((middleTex.getMiddleTex.call(litva)* 100) /100).toFixed(2) + " " + "грн.") 
+console.log("2. Cкільки у середньому податків платять IT-спеціалісти в Україні - ",  ((getMiddleTex.call(ukraine)* 100) /100).toFixed(2) + " " + "грн." + "," + " в Латвіі - ", ((getMiddleTex.call(latvia)* 100) /100).toFixed(2) + " " + "грн." + "," + " в Літві - ", ((getMiddleTex.call(litva)* 100) /100).toFixed(2) + " " + "грн.") 
 
 /*3. Створіть функцію getTotalTaxes.call(country) -> number; – яка рахує, скільки
 всього податків платять IT-спеціалісти у кожній країні. (tax * middleSalary *
 vacancies). Функція повинна викликатись через call та працювати з даними
 через this*/
 
-let countryTexs = {
+    function getCountryTexs (country){
+        return this.tax * this.middleSalary * this.vacancies;
+    }
+
+console.log("3. Cкільки у середньому податків платять IT-спеціалісти в Україні",  Math.floor(getCountryTexs.call(ukraine)* 100) /100 + " " + "грн." + "," + "в Латвіі - ", ((getCountryTexs.call(latvia)* 100) /100).toFixed(2) + " " + "грн." + "," + " в Латвіі - ", ((getCountryTexs.call(litva)* 100) /100 ).toFixed(2) + " " + "грн.")
+
+/* let countryTexs = {
     getCountryTexs: function (country){
         return this.tax * this.middleSalary * this.vacancies;
     }
 } 
 
-console.log("3. Cкільки у середньому податків платять IT-спеціалісти в Україні",  Math.floor(countryTexs.getCountryTexs.call(ukraine)* 100) /100 + " " + "грн." + "," + "в Латвіі - ", ((countryTexs.getCountryTexs.call(latvia)* 100) /100).toFixed(2) + " " + "грн." + "," + " в Латвіі - ", ((countryTexs.getCountryTexs.call(litva)* 100) /100 ).toFixed(2) + " " + "грн.")
+console.log("3. Cкільки у середньому податків платять IT-спеціалісти в Україні",  Math.floor(countryTexs.getCountryTexs.call(ukraine)* 100) /100 + " " + "грн." + "," + "в Латвіі - ", ((countryTexs.getCountryTexs.call(latvia)* 100) /100).toFixed(2) + " " + "грн." + "," + " в Латвіі - ", ((countryTexs.getCountryTexs.call(litva)* 100) /100 ).toFixed(2) + " " + "грн.") */
 
 
 /* 
